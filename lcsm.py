b8103dda8186c11e0164c1388f274718e6716586
alicedalessandro <57675310+alicedalessandro@users.noreply.github.com>
2019-11-26 20:14:17 +0100

    Update lcsm.py

--- lcsm.py
+++ lcsm.py
@@ -1,24 +1,24 @@
 def lcsm(strands):
     strand1=strands[1]
     x=''
-    re=''
+    r=''
     for i in range (len(strand1)):
         for j in range (i+1, len(strand1)+1):
             x=strand1[i:j]
             for h in range(1, len(strands)):
                 if x not in strands[h]:
                     break
-                if h + 1 == len(strands) and len(re) < len(x):
-                    re=x
+                if h + 1 == len(strands) and len(r) < len(x):
+                    r=x
     return re       
 
 with open('rosalind_lcsm.txt') as f:
     A=f.read().split('>Rosalind_')
     l=[]
-    for g in A:
-        g=g.strip()
-        g=g.replace('\n', '')
-        l.append(g)  
+    for j in A:
+        j=j.strip()
+        j=j.replace('\n', '')
+        l.append(j)  
         
 with open('lcsm_out.txt', 'w') as f:
     f.write(lcsm(l))
